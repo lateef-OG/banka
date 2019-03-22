cbb4374b2ae6737884197c6c95242223451cb55d
lateef_og <lateef@cchubnigeria.com>
2019-03-22 12:49:18 +0100

    create account page done

--- UI/js/app.js
+++ UI/js/app.js
@@ -13,31 +13,3 @@ function closeNav() {
     document.getElementById("sidebar").style.marginLeft = "-3px";
     // document.body.style.backgroundColor = "#ffffff";
 }
-/** 
- * Script for modal
-*/
-// Get the modal
-var modal = document.getElementById('modal');
-
-// Get the button that opens the modal
-var btn = document.getElementById("openModal");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks the button, open the modal 
-btn.onclick = function() {
-  modal.style.display = "flex";
-}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
