555be1fa3bec01cb374b91c5b19de92c207ea653
lateef_og <lateef@cchubnigeria.com>
2019-03-29 13:50:28 +0100

    set up express with nodemon and babel for transpiling

---
+++ Api/src/index.js
@@ -0,0 +1,28 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+  res.json({
+    message: 'Welcome to Banka API',
+  });
+});
+
+app.use((req, res, next) => {
+  const error = new Error('Not found');
+  error.status = 404;
+  next(error);
+});
+
+app.use((error, req, res, next) => {
+  res.status(error.status || 500).json({
+    error: {
+      message: error.message,
+    },
+  });
+});
+
+export default app;
---
+++ Api/src/server.js
@@ -0,0 +1,6 @@
+import app from './index';
+
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT);
+console.log(`App is running on port:${PORT}`);
