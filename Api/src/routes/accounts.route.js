64d872f5e05e6083b0c054babd31c6723408cce2
lateef_og <lateef@cchubnigeria.com>
2019-04-02 14:31:08 +0100

    created route for listing accounts and tested with postman

--- Api/src/routes/accounts.route.js
+++ Api/src/routes/accounts.route.js
@@ -3,6 +3,7 @@ import AccountController from '../controllers/account.controller';
 
 const router = Router();
 
+router.get('/', AccountController.listAccounts);
 router.post('/create-account', AccountController.createAccount);
 
 export default router;
